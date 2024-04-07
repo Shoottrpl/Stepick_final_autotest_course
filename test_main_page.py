3400da1893ecdb6b6bae370beb0e31c6012fb1fb
Shoottrpl <triplsix1337@gmail.com>
2024-04-07 22:04:08 +0300

    refactoring test_main_page.py like Page Object style

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,9 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
-def got_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    got_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.got_to_login_page()
