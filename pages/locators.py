fa21890a44480929024dee3d5b1e6e4a869ee484
Shoottrpl <triplsix1337@gmail.com>
2024-04-25 23:44:21 +0300

    refactor Base class add new methods

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,19 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
 class ProductPageLocators():
     ADD_IN_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
-    PRODUCT_PRICE = (By.CSS_SELECTOR,'.price_color')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.price_color')
     SUCCESS_ADD_MESSAGE = (By.CSS_SELECTOR, '#messages .alert')
     SUCCESS_ADD_MESSAGE_NAME = (By.CSS_SELECTOR, '#messages strong')
--- test_product_page.py
+++ test_product_page.py
@@ -42,3 +42,14 @@ class TestProductPage:
         page.add_to_basket()
         page.should_be_disappeared_message()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
